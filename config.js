5e3492bf3f29469bffdceb7bcb1abec661833bb2
ChiragChakrabortty <chiragchakrabortty93@gmail.com>
2021-03-23 20:38:59 +0530

    Initial Configuration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: '',
+    FB_PAGE_TOKEN: 'EAAMfK5ElZCwABAFRm9ZAkLd7Mw8ak9OTgsk6GNLzegSzTVGW6hrQURDGcSdUKNYCuasGPzHJG2kSVxQ5vGK6sgYLTOjbZBWGvSK3pjiWSSGRRLAIh4V5o2kslI9zsHZAU8jK4RxvHaPbVjobMUIZBgcmfXcZCvZALkkmdinQH9F1iNWBRS3qFml0MEBhn2zdEUZD',
+    FB_VERIFY_TOKEN: 'ynwaynwaynwaynwa',
+    FB_APP_SECRET: '1add8b43261699eeebdc94fb85c2853c',
+    SERVER_URL: 'https://mentalitymonsters.herokuapp.com/',
     GOOGLE_PROJECT_ID: '',
     DF_LANGUAGE_CODE: '',
     GOOGLE_CLIENT_EMAIL: '',
